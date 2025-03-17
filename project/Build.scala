d03c9add542d94957b434bf4a68cfdea054460c8
Jerome Prinet <jprinet@gradle.com>
2025-03-17 11:57:58 +0100

    Save local cache

--- project/Build.scala
+++ project/Build.scala
@@ -354,7 +354,7 @@ object Build {
         .withBuildCache(
           buildCache
             .withLocal(buildCache.local.withEnabled(true).withStoreEnabled(true))
-            .withRemote(buildCache.remote.withEnabled(true).withStoreEnabled(isInsideCI))
+            .withRemote(buildCache.remote.withEnabled(true).withStoreEnabled(false))
             .withRequireClean(!isInsideCI)
         )
         .withTestRetry(
