25922b882a1733932eaf671c731e45d445d5c6a1
Jerome Prinet <jprinet@gradle.com>
2025-03-19 10:21:11 +0100

    update local cache dir

--- project/Build.scala
+++ project/Build.scala
@@ -354,7 +354,7 @@ object Build {
         .withBuildCache(
           buildCache
 //            .withLocal(buildCache.local.withEnabled(true).withStoreEnabled(true))
-            .withLocal(buildCache.local.withEnabled(true).withStoreEnabled(true).withDirectory(Some(file("/tmp/develocity-cache"))))
+            .withLocal(buildCache.local.withEnabled(true).withStoreEnabled(true).withDirectory(Some(file("/tmp/.develocity-cache"))))
             .withRemote(buildCache.remote.withEnabled(false).withStoreEnabled(false))
             .withRequireClean(false)
         )
